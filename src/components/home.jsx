7dced9c3952fe5e420e297cdbfced29e2796f2db
chiragsinhchavda <chiragsinhchavda01@gmail.com>
2025-02-23 22:51:22 +0530

    changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
                         <motion.h1 variants={container(1.5)} initial="hidden" animate="visible" className="pb-10 text-5xl font-thin tracking-tight lg:mt-10 lg:text-6xl">
-                            Chiragsinh Chavda
+                            Chirag Chavda
                         </motion.h1>
                         <motion.span variants={container(1.5)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent">Full Stack Developer</motion.span>
                         <motion.p variants={container(1.5)} initial="hidden" animate="visible" className="my-1 max-w-xl py-5 font-light tracking-tight">{HOME_CONTENT}</motion.p>
